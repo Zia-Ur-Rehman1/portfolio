fef5bc83d55375a7d155cef9c29a34b94a20705b
Zia-Ur-Rehman1 <zia.rehman.web@gmail.com>
2025-09-16 18:38:15 +0500

    Updated Header links navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,52 @@
-import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { Container, Div2, Div3, NavLink, SocialContainer, SocialIcons } from './HeaderStyles';
 
-const Header = () => (
-  <Container>
-    <Div2>
-      <li>
-        <Link href="#projects" scroll={false}>
-          <NavLink>Projects</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#skills" scroll={false}>
-          <NavLink>Skills</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about" scroll={false}>
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
+const Header = () => {
+  const handleSmoothScroll = (e, targetId) => {
+    e.preventDefault();
+    const element = document.getElementById(targetId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      window.history.pushState(null, null, `#${targetId}`);
+    }
+  };
 
-    <Div3>
-      <SocialContainer>
-        <SocialIcons href="https://github.com/Zia-Ur-Rehman1" target="_blank" rel="noopener noreferrer">
-          <AiFillGithub size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://www.linkedin.com/in/zia1/" target="_blank" rel="noopener noreferrer">
-          <AiFillLinkedin size="3rem" />
-        </SocialIcons>
-      </SocialContainer>
-    </Div3>
-  </Container>
-);
+  return (
+    <Container>
+      <Div2>
+        <li>
+          <NavLink href="#projects" onClick={(e) => handleSmoothScroll(e, 'projects')}>
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href="#skills" onClick={(e) => handleSmoothScroll(e, 'skills')}>
+            Skills
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href="#about" onClick={(e) => handleSmoothScroll(e, 'about')}>
+            About
+          </NavLink>
+        </li>
+      </Div2>
+
+      <Div3>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Zia-Ur-Rehman1" target="_blank" rel="noopener noreferrer">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/zia1/" target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </Div3>
+    </Container>
+  );
+};
 
 export default Header;
